24f3fde0bbe8c29ff42be212add4d3bbbf81659d
MarcinZbor <marcinzb@gazeta.pl>
2023-06-17 12:27:31 +0200

    fixed@ll

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,43 +4,48 @@ import { useEffect } from 'react';
 import styles from './ContactList.module.css';
 import { selectLoading } from 'redux/selectors';
 import { getContactsOperation, deleteContactOperation } from 'redux/operations';
+import { createSelector } from '@reduxjs/toolkit';
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
+  // const contacts = useSelector(selectContacts);
+  // const filter = useSelector(selectFilter);
   const loading = useSelector(selectLoading);
   const dispatch = useDispatch();
 
-  const filteredContacts = () =>
-    filter
-      ? contacts.filter(contact =>
-          contact.name.toLowerCase().includes(filter.toLowerCase())
-        )
-      : contacts;
+  const filteredContactsSelector = createSelector(
+    [selectContacts, selectFilter],
+    (contacts, filter) =>
+      filter
+        ? contacts.filter(contact =>
+            contact.name.toLowerCase().includes(filter.toLowerCase())
+          )
+        : contacts
+  );
 
-      useEffect(() => {
-        dispatch(getContactsOperation());
+  const filteredContacts = useSelector(filteredContactsSelector);
 
-      }, [dispatch])
+  useEffect(() => {
+    dispatch(getContactsOperation());
+  }, [dispatch]);
 
-      return (
-        <>
-        <ul className={styles.list}>
-          {contacts.length !== 0 &&
-            filteredContacts().map(({ id, name, phone }) => (
-              <li key={id} className={styles.item}>
-                <p className={styles.p}>
-                  {name}: {phone}
-                </p>
-                <button type="button" onClick={() => dispatch(deleteContactOperation(id))}>
-                  Delete
-                </button>
-              </li>
-            ))}
-        </ul>
-        {loading && <h3>Loading...</h3>}
-        </>
-      );
-    };
+  return (
+    <>
+      <ul className={styles.list}>
+        {filteredContacts.length !== 0 &&
+          filteredContacts.map(({ id, name, phone }) => (
+            <li key={id} className={styles.item}>
+              <p className={styles.p}>
+                {name}: {phone}
+              </p>
+              <button type="button" onClick={() => dispatch(deleteContactOperation(id))}>
+                Delete
+              </button>
+            </li>
+          ))}
+      </ul>
+      {loading && <h3>Loading...</h3>}
+    </>
+  );
+};
 
 export default ContactList;
